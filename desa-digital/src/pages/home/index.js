7351063e7dfe3f5ba542fd20ea26338167cbd12f
seccret404 <edwardtua25@gmail.com>
2024-06-20 09:03:11 +0700

    Update Home

--- desa-digital/src/pages/home/index.js
+++ desa-digital/src/pages/home/index.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image, ScrollView } from 'react-native'
-import Footer from '../../components/layout/footer'
-import HeaderHome from '../../components/layout/headerHome'
-import UserIcon from '../../components/icon/user'
-import Underline from '../../components/icon/underline'
-import AgendaIcon from '../../components/icon/agenda'
-import OrganisasiIcon from '../../components/icon/organisasi'
-import PengumumanIcon from '../../components/icon/pengumuman'
-import ApbdesIcon from '../../components/icon/apbdes'
-import PendudukIcon from '../../components/icon/penduduk'
-import BeritaIcon from '../../components/icon/berita'
-import { useEffect } from 'react'
-import { fetchDusun, getBerita, getPengumuman } from '../../services/desaDigital.services'
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Image } from 'react-native';
+import Footer from '../../components/layout/footer';
+import HeaderHome from '../../components/layout/headerHome';
+import UserIcon from '../../components/icon/user';
+import Underline from '../../components/icon/underline';
+import AgendaIcon from '../../components/icon/agenda';
+import OrganisasiIcon from '../../components/icon/organisasi';
+import PengumumanIcon from '../../components/icon/pengumuman';
+import ApbdesIcon from '../../components/icon/apbdes';
+import PendudukIcon from '../../components/icon/penduduk';
+import BeritaIcon from '../../components/icon/berita';
+import { fetchDusun, getBerita, getPengumuman } from '../../services/desaDigital.services';
+
 export default function Home({ navigation }) {
      const [numColumns, setNumColumns] = useState(3);
      const [userList, setUserList] = useState([]);
-     const [pengumuman,setPengumuman] = useState([]);
+     const [pengumuman, setPengumuman] = useState([]);
      const [berita, setBerita] = useState([]);
 
      useEffect(() => {
@@ -25,24 +25,24 @@ export default function Home({ navigation }) {
                     const latestBerita = allBerita.slice(0, 3);
                     setBerita(latestBerita);
                } catch (error) {
-                    console.error('Error fetching user list:', error);
+                    console.error('Error fetching berita list:', error);
                }
-          }
+          };
           fetchBerita();
-     }, [])
-     useEffect(() =>{
-          const fetchPengumuman = async () =>{
-               try{
+     }, []);
+
+     useEffect(() => {
+          const fetchPengumuman = async () => {
+               try {
                     const data = await getPengumuman();
-                    const latestData = data.slice(0,3);
+                    const latestData = data.slice(0, 3);
                     setPengumuman(latestData);
+               } catch (error) {
+                    console.error('Error fetching Pengumuman:', error);
                }
-               catch (error) {
-                    console.error('Error fetching user list:', error);
-               }
-          }
+          };
           fetchPengumuman();
-     },[])
+     }, []);
 
      useEffect(() => {
           const fetchData = async () => {
@@ -50,12 +50,12 @@ export default function Home({ navigation }) {
                     const data = await fetchDusun();
                     setUserList(data);
                } catch (error) {
-                    console.error('Error fetching user list:', error);
+                    console.error('Error fetching dusun list:', error);
                }
           };
-
           fetchData();
      }, []);
+
      const menu = [
           { key: 1, icon: <AgendaIcon />, namaMenu: 'Agenda Desa', route: 'agenda' },
           { key: 2, icon: <OrganisasiIcon />, namaMenu: 'Organisasi Desa', route: 'organisasi' },
@@ -78,7 +78,8 @@ export default function Home({ navigation }) {
                     </Text>
                </View>
           </TouchableOpacity>
-     )
+     );
+
      const formatDate = (tgl) => {
           const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
           return new Date(tgl).toLocaleDateString('id-ID', options);
@@ -93,101 +94,114 @@ export default function Home({ navigation }) {
           if (text.length <= maxLength) return text;
           return text.substr(0, maxLength) + '...';
      };
+
+     const data = [
+          { key: 'menu', type: 'menu' },
+          { key: 'beritaTitle', type: 'title', title: 'Berita Desa terbaru' },
+          ...berita.map(beritaData => ({ key: `berita-${beritaData.id}`, type: 'berita', data: beritaData })),
+          { key: 'pengumumanTitle', type: 'title', title: 'Agenda Desa terbaru' },
+          ...pengumuman.map(pengumumanData => ({ key: `pengumuman-${pengumumanData.id}`, type: 'pengumuman', data: pengumumanData }))
+     ];
+
+     const renderItem = ({ item }) => {
+          if (item.type === 'menu') {
+               return (
+                    <FlatList
+                         data={menu}
+                         renderItem={renderMenu}
+                         numColumns={numColumns}
+                         keyExtractor={(item) => item.key.toString()}
+                         scrollEnabled={false}
+                    />
+               );
+          } else if (item.type === 'title') {
+               return (
+                    <View style={styles.titleNews}>
+                         <Text style={styles.textTitleNews}>{item.title}</Text>
+                    </View>
+               );
+          } else if (item.type === 'berita') {
+               const beritaData = item.data;
+               return (
+                    <View style={styles.cardNews} key={beritaData.id}>
+                         <View style={{ paddingLeft: 16, paddingTop: 7, paddingBottom: 7 }}>
+                              <Image source={{ uri: `https://api-admin.desasosordolok.id/images/cover/${beritaData.cover}` }} style={{ width: 95, height: 95, borderRadius: 5 }} />
+                         </View>
+                         <View style={styles.contentNews}>
+                              <Text style={styles.judul}>{beritaData.judul_berita}</Text>
+                              <Text style={styles.waktu}>{formatDate(beritaData.tgl_publikasi)}</Text>
+                              <Text style={styles.deskripsi}>{truncateText(cleanHTMLTags(beritaData.isi_berita), 32)}</Text>
+                              <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', width: 195 }}>
+                                   <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('detail-berita', { id: beritaData.id })}>
+                                        <Text style={styles.btnText}>Selengkapnya</Text>
+                                   </TouchableOpacity>
+                              </View>
+                         </View>
+                    </View>
+               );
+          } else if (item.type === 'pengumuman') {
+               const pengumumanData = item.data;
+               return (
+                    <View style={styles.cardNews} key={pengumumanData.id}>
+                         <View style={{ paddingLeft: 16, paddingTop: 7, paddingBottom: 7, width:100  }}>
+                              <Image source={require('../../../assets/favicon.png')} width={12} />
+                         </View>
+                         <View style={styles.contentNews}>
+                              <Text style={styles.judul}>{pengumumanData.judul_pengumuman}</Text>
+                              <Text style={styles.waktu}>{formatDate(pengumumanData.tgl_publikasi)}</Text>
+                              <Text style={styles.deskripsi}>{truncateText(cleanHTMLTags(pengumumanData.deskripsi_pengumuman), 32)}</Text>
+                              <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', width: 195 }}>
+                                   <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('detail-pengumuman', { id: pengumumanData.id })}>
+                                        <Text style={styles.btnText}>Selengkapnya</Text>
+                                   </TouchableOpacity>
+                              </View>
+                         </View>
+                    </View>
+               );
+          }
+          return null;
+     };
+
      return (
           <View style={styles.container}>
                <HeaderHome />
-               <ScrollView>
-                    <View style={styles.content}>
-                         <View style={styles.dataStatistik}>
-                              <View style={styles.dataPenduduk}>
-                                   <View style={styles.box}>
-                                        <Text style={styles.title}>Total Penduduk</Text>
-                                        <View style={styles.countP}>
-                                             <UserIcon color='#ffffff' />
-                                             <Text style={styles.count}>3.567 Jiwa</Text>
+               <FlatList
+                    data={data}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.key}
+                    ListHeaderComponent={
+                         <View style={styles.content}>
+                              <View style={styles.dataStatistik}>
+                                   <View style={styles.dataPenduduk}>
+                                        <View style={styles.box}>
+                                             <Text style={styles.title}>Total Penduduk</Text>
+                                             <View style={styles.countP}>
+                                                  <UserIcon color='#ffffff' />
+                                                  <Text style={styles.count}>3.567 Jiwa</Text>
+                                             </View>
+                                        </View>
+                                   </View>
+                                   <View style={styles.dataPenduduk}>
+                                        <View style={styles.box}>
+                                             <Text style={styles.title}>Statistik Penduduk</Text>
+                                             <View style={styles.countLP}>
+                                                  <Text style={{ fontSize: 12, color: '#ffffff', width: 62 }}>1.345 Jiwa  Perempuan</Text>
+                                                  <Text style={{ fontSize: 40, color: '#ffffff', }}>|</Text>
+                                                  <Text style={{ fontSize: 12, color: '#ffffff', width: 62 }}>1.345 Jiwa Laki-laki</Text>
+                                             </View>
                                         </View>
                                    </View>
                               </View>
-                              <View style={styles.dataPenduduk}>
-                                   <View style={styles.box}>
-                                        <Text style={styles.title}>Statistik Penduduk</Text>
-                                        <View style={styles.countLP}>
-                                             <Text style={{ fontSize: 12, color: '#ffffff', width: 62 }}>1.345 Jiwa  Perempuan</Text>
-                                             <Text style={{ fontSize: 40, color: '#ffffff', }}>|</Text>
-                                             <Text style={{ fontSize: 12, color: '#ffffff', width: 62 }}>1.345 Jiwa Laki-laki</Text>
-                                        </View>
-                                   </View>
+                              <View style={styles.Underline}>
+                                   <Underline />
                               </View>
+                              <Text style={{ fontSize: 18, fontWeight: '700', paddingLeft: 20, marginTop: 15 }}>Informasi Desa</Text>
                          </View>
-                         {/* garis bawah */}
-                         <View style={styles.Underline}>
-                              <Underline />
-                         </View>
-                         <Text style={{ fontSize: 18, fontWeight: '700', paddingLeft: 20, marginTop: 15 }}>Informasi Desa</Text>
-                         {/* menu container */}
-                         <View >
-                              <FlatList
-                                   data={menu}
-                                   renderItem={renderMenu}
-                                   numColumns={numColumns}
-                              />
-                         </View>
-                         <View>
-                              <View style={styles.titleNews}>
-                                   <Text style={styles.textTitleNews}>Berita Desa terbaru</Text>
-                              </View>
-                              <View style={styles.boxCard}>
-                                   {/* berita desa */}
-                                   {berita.map(beritaData => (
-                                        <View style={styles.cardNews} key={beritaData.id}>
-                                             <View style={{ paddingLeft: 16, paddingTop: 7, paddingBottom: 7 }}>
-                                                  <Image source={{ uri: `https://api-admin.desasosordolok.id/images/cover/${beritaData.cover}` }} style={{ width: 95, height: 95, borderRadius: 5 }} />
-                                             </View>
-                                             <View style={styles.contentNews}>
-                                                  <Text style={styles.judul}>{beritaData.judul_berita}</Text>
-                                                  <Text style={styles.waktu}>{formatDate(beritaData.tgl_publikasi)}</Text>
-                                                  <Text style={styles.deskripsi}>{truncateText(cleanHTMLTags(beritaData.isi_berita), 32)}</Text>
-                                                  <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', width: 195 }}>
-                                                  <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('detail-berita', { id: beritaData.id })}>
-                                                            <Text style={styles.btnText}>Selengkapnya</Text>
-                                                       </TouchableOpacity>
-                                                  </View>
-                                             </View>
-                                        </View>
-                                   ))}
-
-                              </View>
-                              {/* Agenda Desa */}
-                              <View style={styles.titleNews}>
-                                   <Text style={styles.textTitleNews}>Agenda Desa terbaru</Text>
-                              </View>
-                              <View style={styles.boxCard}>
-                                   {pengumuman.map(pengumumanData =>(
-                                        <View style={styles.cardNews}>
-                                        <View style={{ paddingLeft: 16, paddingTop: 7, paddingBottom: 7 }}>
-                                             <Image source={{ uri: `https://api-admin.desasosordolok.id/pengumuman/images/cover/${pengumumanData.cover_pengumuman}` }} width={95} height={95} borderRadius={5} />
-                                        </View>
-                                        <View style={styles.contentNews}>
-                                             <Text style={styles.judul}>{pengumumanData.judul_pengumuman}</Text>
-                                             <Text style={styles.waktu}>{formatDate(pengumumanData.tgl_publikasi)}</Text>
-                                             <Text style={styles.deskripsi}>{truncateText(cleanHTMLTags(pengumumanData.deskripsi_pengumuman), 32)}</Text>
-                                             <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', width: 195 }}>
-                                             <TouchableOpacity style={styles.btn} onPress={() => navigation.navigate('detail-pengumuman', { id: pengumumanData.id })}>
-                                                       <Text style={styles.btnText}>Selengkapnya</Text>
-                                                  </TouchableOpacity>
-                                             </View>
-                                        </View>
-                                   </View> 
-                                   ))}
-                                  
-                              </View>
-                         </View>
-
-                    </View>
-               </ScrollView>
+                    }
+               />
                <Footer navigation={navigation} />
           </View>
-     )
+     );
 }
 
 const styles = StyleSheet.create({
@@ -199,7 +213,6 @@ const styles = StyleSheet.create({
      },
      content: {
           flex: 1,
-
           marginBottom: 10
      },
      dataPenduduk: {
@@ -224,7 +237,6 @@ const styles = StyleSheet.create({
           alignItems: 'center',
           marginTop: 10
      },
-
      countP: {
           display: 'flex',
           flexDirection: 'row',
@@ -267,12 +279,10 @@ const styles = StyleSheet.create({
           height: 23,
           marginLeft: 15,
           marginTop: 15
-
      },
      textTitleNews: {
           textAlign: 'center',
           color: '#0D9276',
-
      },
      judul: {
           fontSize: 12,
@@ -301,7 +311,6 @@ const styles = StyleSheet.create({
      },
      cardNews: {
           width: 330,
-
           display: 'flex',
           flexDirection: 'row',
           backgroundColor: '#ffffff',
@@ -324,9 +333,5 @@ const styles = StyleSheet.create({
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center'
-
      }
-
-
-
-})
+});
