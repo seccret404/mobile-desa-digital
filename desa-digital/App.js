32dbe6155ee92bc3305f99c946b0ca8d9b2e1304
seccret404 <edwardtua25@gmail.com>
2024-07-08 19:39:20 +0700

    The Final Chapter App Desa Digital

--- desa-digital/App.js
+++ desa-digital/App.js
@@ -1,7 +1,9 @@
 // App.js
 import React from 'react';
 import AppNavigation from './src/navigation/index';
+import { enableScreens } from 'react-native-screens';
 
+enableScreens(); 
 export default function App() {
   return (
     <>
